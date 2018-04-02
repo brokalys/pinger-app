fbcabccb62d2d0257140434b724b81b050a04878
Matiss Janis Aboltins <matiss@mja.lv>
2018-04-02 13:41:36 +0300

    Remove key from dev env

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  GMAPS_KEY: '"AIzaSyDR3WZeo7IIIIPPb114g-kyl3zgdGotsL0"',
+  GMAPS_KEY: '""',
 })
