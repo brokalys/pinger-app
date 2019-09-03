6b7a1770bd2f1143f25d5ff7f403cd5628134e72
Matiss Janis Aboltins <matiss@mja.lv>
2019-09-03 09:08:52 +0300

    Updates

---
+++ precache-manifest.0e6441cf89586386c2c819b4fd47f6c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "db456f6bdcfc2c3632f7",
+    "url": "/css/app.bb33669e.css"
+  },
+  {
+    "revision": "db456f6bdcfc2c3632f7",
+    "url": "/js/app.9526416b.js"
+  },
+  {
+    "revision": "054ac305580fc903edfc",
+    "url": "/css/chunk-vendors.06cadd5e.css"
+  },
+  {
+    "revision": "054ac305580fc903edfc",
+    "url": "/js/chunk-vendors.18655819.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "699e71ab5e112e5a20e2b5ccf0ab7b5f",
+    "url": "/index.html"
+  }
+];
