3c54d22fc103fe92e16ce25282df0ce37e09133f
Matiss Janis Aboltins <matiss@mja.lv>
2019-09-07 09:09:46 +0300

    Updates

--- precache-manifest.7f797970da882822bc0b2900800eef06.js
+++ precache-manifest.7f797970da882822bc0b2900800eef06.js
@@ -8,23 +8,23 @@ self.__precacheManifest = [
     "url": "/js/chunk-vendors.aa0aa3dd.js"
   },
   {
-    "revision": "1173e8f6615c3a917f4b",
+    "revision": "2ccf9aec5600f0fc69cd",
     "url": "/css/index.bb33669e.css"
   },
   {
-    "revision": "1173e8f6615c3a917f4b",
-    "url": "/js/index.59286969.js"
-  },
-  {
-    "revision": "732389ded34cb9c52dd88271f1345af9",
-    "url": "/fonts/element-icons.732389de.ttf"
+    "revision": "2ccf9aec5600f0fc69cd",
+    "url": "/js/index.de93c012.js"
   },
   {
     "revision": "535877f50039c0cb49a6196a5b7517cd",
     "url": "/fonts/element-icons.535877f5.woff"
   },
   {
-    "revision": "39a0ed4fe66416c03d116b0b8a4c36df",
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "40c7dab8b7a6ae314402e7826fef98fa",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4f5dc597d9ffc6840946e2efa7e09be8.js"
+  "/precache-manifest.7f797970da882822bc0b2900800eef06.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "pinger"});
