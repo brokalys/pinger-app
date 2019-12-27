bfa806fb37f395ba23ed96cabd2785efa6b4c66b
Matiss Janis Aboltins <matiss@mja.lv>
2019-12-27 12:29:24 +0200

    Updates

---
+++ precache-manifest.ac132cadcacfe2d7f2ac66c5a43d6892.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a50eee979d22463ce1",
+    "url": "/css/chunk-vendors.6bd32be9.css"
+  },
+  {
+    "revision": "a6604a7eb0f0ce982507",
+    "url": "/css/index.bb33669e.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "1b06c2a09464a508c4ed8a0ef5ffa617",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06a50eee979d22463ce1",
+    "url": "/js/chunk-vendors.d83699f2.js"
+  },
+  {
+    "revision": "a6604a7eb0f0ce982507",
+    "url": "/js/index.32d23e36.js"
+  },
+  {
+    "revision": "779ffb3201057096532a780deef023db",
+    "url": "/manifest.json"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1880f9344a41f11f29f318f547392b43.js"
+  "/precache-manifest.ac132cadcacfe2d7f2ac66c5a43d6892.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "pinger"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,4 @@ workbox.core.setCacheNameDetails({prefix: "pinger"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
