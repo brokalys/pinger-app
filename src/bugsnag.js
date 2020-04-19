3ce81a2a543963154be711cc28fbdc1b25b29d28
Matiss Janis Aboltins <matiss@mja.lv>
2020-04-19 14:57:43 +0100

    fix: bugsnag API key env variable

--- src/bugsnag.js
+++ src/bugsnag.js
@@ -2,9 +2,9 @@ import Vue from "vue";
 import Bugsnag from "@bugsnag/js";
 import BugsnagPluginVue from "@bugsnag/plugin-vue";
 
-if (process.env.VUA_BUGSNAG_KEY) {
+if (process.env.VUE_APP_BUGSNAG_KEY) {
   Bugsnag.start({
-    apiKey: process.env.VUA_BUGSNAG_KEY,
+    apiKey: process.env.VUE_APP_BUGSNAG_KEY,
     plugins: [new BugsnagPluginVue(Vue)],
   });
 }
