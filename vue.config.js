172bc1ac35779a663bd438c9df1cf66f513b60b1
Matiss Janis Aboltins <matiss@mja.lv>
2020-04-19 18:55:01 +0100

    ci: slightly improved build step

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,19 @@
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   pages: {
     index: {
       entry: 'src/main.js',
       template: 'index.html',
     },
+  },
+  configureWebpack: {
+    plugins: [
+      new CopyPlugin([
+        'static/**',
+        'CNAME'
+      ]),
+    ],
   },
   pluginOptions: {
     apollo: {
