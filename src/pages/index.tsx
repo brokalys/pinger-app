0e56e26363ee26c1daffc330f560131ed18b4621
Matiss Janis Aboltins <matiss@mja.lv>
2021-02-27 11:47:08 +0000

    :bug:  swap over to hash-router for usage in github pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import ScrollToTop from "react-router-scroll-top";
 import PageContainer from "components/PageContainer";
 import Home from "./Home";
